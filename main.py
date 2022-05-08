64a07362b7ca289c0c291ccbfad14a656b009cf6
unknown <mindru.ion97@gmail.com>
2022-05-08 17:29:13 +0300

    Added my project

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 from telegram.ext import Updater
 from pydantic import BaseModel
 
 app = FastAPI()
 updater = Updater("5335002137:AAE4xLCtIeean8hVOSsR32l3DGlZjaRKaOo", arbitrary_callback_data=True)
 
+origins = [
+    "http://localhost:63342/",
+    "http://localhost:63342",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class Data(BaseModel):
-    full_name: str
+    name: str
     email: Optional[str] = 'nu a trimis numarul'
-    phone_number: str
+    phone: str
     message: str
 
 
 def make_text(data: Data):
     return f"""
-        \n*_Numele si prenumele_*:  {data.full_name}\n*_Email\-ul_*:  {data.email}\n*_Numarul de telefon_*:  {data.phone_number}\n*_Mesajul_*:  {data.message}\n
+        \n<strong><b><i><u>Numele si prenumele</u></i></b></strong>:  {data.name}\n\n<strong><b><i><u>Email-ul</u></i></b></strong>:  {data.email}\n\n<strong><b><i><u>Numarul de telefon</u></i></b></strong>:  {data.phone}\n\n<strong><b><i><u>Mesajul</u></i></b></strong>:  {data.message}\n
     """
 
 
 @app.post("/send_email")
 async def send_email(data: Data):
-    updater.bot.sendMessage(chat_id='730014397', text=make_text(data), parse_mode='MarkdownV2')
+    updater.bot.sendMessage(chat_id='730014397', text=make_text(data), parse_mode='HTML')
     return {"message": "success"}
